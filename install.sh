0551689cd5d5157f900f818adc3f1c4ba398cff2
AuraDev <deepa.lovi26@gmail.com>
2025-08-27 23:44:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ clear
 echo -e "${CYAN}"
 echo "╔═══════════════════════════════════════╗"
 echo "║      VPS Deploy Bot Installer 🚀      ║"
-echo "║         Made By Advik     ║"
+echo "║         Made By Advik / AuraDev "
 echo "╚═══════════════════════════════════════╝"
 echo -e "${NC}"
 sleep 2
